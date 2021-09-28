7235f3c70fc2608f5030733e2ceccd5ca6cd32d0
Rayen RAFFA <raffa.rayen@gmail.com>
2021-09-28 13:01:29 +0100

    add initial register state

---
+++ src/app/store/state/register.state.ts
@@ -0,0 +1,48 @@
+import {
+  IPersonalInformation,
+  IAddress,
+  IPaymentInformation,
+} from 'src/app/models/registration-information.interface';
+import { IRegistrationQuestions } from 'src/app/models/registration-questions.interface';
+
+export interface IRegisterState {
+    currentRegistrationStep: string,
+    personalInfo: IPersonalInformation,
+    address: IAddress,
+    paymentInfo: IPaymentInformation,
+    regisrationQuestions: IRegistrationQuestions
+}
+
+// TODO : create initial register questions
+export const initialPersonalInfoState: IPersonalInformation = {
+    firstName: '',
+    lastName: '',
+    telephone: ''
+}
+
+export const initialAddressState: IAddress = {
+    street: '',
+    houseNumber: '',
+    zipCode: '',
+    city: ''
+}
+
+export const initialPaymentInfo: IPaymentInformation = {
+    owner: '',
+    iban: '',
+    paymentDataId: ''
+}
+
+export const initialRegistrationQuestionsState: IRegistrationQuestions = {
+    personalInfo: [],
+    address: [],
+    paymentInfo: []
+}
+
+export const inititalRegisterState: IRegisterState = {
+    currentRegistrationStep: 'Not Started',
+    personalInfo: initialPersonalInfoState,
+    address: initialAddressState,
+    paymentInfo: initialPaymentInfo,
+    regisrationQuestions: initialRegistrationQuestionsState
+}
