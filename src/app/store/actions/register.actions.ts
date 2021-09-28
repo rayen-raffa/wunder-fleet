f978696dfdce94c0f2f015b15bbbd51b029a5552
Rayen RAFFA <raffa.rayen@gmail.com>
2021-09-28 15:19:32 +0100

    create register actions

---
+++ src/app/store/actions/register.actions.ts
@@ -0,0 +1,46 @@
+import { Action } from '@ngrx/store';
+
+import {
+  IPersonalInformation,
+  IAddress,
+  IPaymentInformation,
+} from 'src/app/models/registration-information.interface';
+
+export enum ERegisterActions {
+  StartRegister = '[Register] Start',
+  SubmitPersonalInformation = '[Register] Submit Personal Information',
+  SubmitAddress = '[Register] Submit Address',
+  SubmitPaymentInformatioin = '[Register] Submit Payment Information',
+  RegisterSuccess = '[Register] Registration Successful',
+}
+
+export class StartRegister implements Action {
+  public readonly type = ERegisterActions.StartRegister;
+}
+
+export class SubmitPersonalInformation implements Action {
+  public readonly type = ERegisterActions.SubmitPersonalInformation;
+  constructor(public payload: IPersonalInformation) {}
+}
+
+export class SubmitAddress implements Action {
+  public readonly type = ERegisterActions.SubmitAddress;
+  constructor(public payload: IAddress) {}
+}
+
+export class SubmitPaymentInformatioin implements Action {
+  public readonly type = ERegisterActions.SubmitPaymentInformatioin;
+  constructor(public payload: IPaymentInformation) {}
+}
+
+export class RegisterSuccess implements Action {
+  public readonly type = ERegisterActions.RegisterSuccess;
+  constructor(public payload: IPaymentInformation['paymentDataId']) {}
+}
+
+export type RegisterActions =
+  | StartRegister
+  | SubmitPersonalInformation
+  | SubmitAddress
+  | SubmitPaymentInformatioin
+  | RegisterSuccess;
