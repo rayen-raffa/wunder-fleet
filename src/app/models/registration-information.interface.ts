cbd16ba910959006fe1d2f2021f708ecc33b4dbe
Rayen RAFFA <raffa.rayen@gmail.com>
2021-09-28 12:42:35 +0100

    add registration information interfac

---
+++ src/app/models/registration-information.interface.ts
@@ -0,0 +1,24 @@
+export interface IPersonalInformation {
+    firstName: string,
+    lastName: string,
+    telephone: string
+}
+
+export interface IAddress {
+    street: string,
+    houseNumber: string,
+    zipCode: number,
+    city: string
+}
+
+export interface IPaymentInformation {
+    owner: string,
+    iban: string,
+    paymentDataId: string
+}
+
+export interface IRegisrationInformation {
+    personalInfo: IPersonalInformation,
+    address: IAddress,
+    paymentInfo: IPaymentInformation
+}
